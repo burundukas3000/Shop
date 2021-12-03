5d4d6276fb4aaf6f0a7cb8fc3ee2a335584f8ecd
biaba <aprily@inbox.lv>
2021-12-03 16:53:45 +0200

    initial spring

---
+++ src/main/java/com/project/backend/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.project.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/error").permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin**").hasRole("ADMIN")
+                .antMatchers("/customer**").hasRole("CUSTOMER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic().and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager()
+    {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+        userDetailsList.add(User.withUsername("admin").password(passwordEncoder().encode("admin"))
+                .roles("ADMIN", "USER").build());
+        userDetailsList.add(User.withUsername("customer").password(passwordEncoder().encode("customer"))
+                .roles("CUSTOMER", "USER").build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/project/backend/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.project.backend.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        String username = authentication.getName();
+        Object principal = authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        model.addAttribute("name", username);
+        model.addAttribute("role", authorities.toArray()[0]);
+        return "home.jsp";
+    }
+}
